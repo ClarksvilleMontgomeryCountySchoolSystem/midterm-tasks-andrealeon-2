65120fbf937f335fb9b82dcf06ea80be356c909c
andrealeon-2 <andrea.leon270@students.cmcss.net>
2025-12-18 08:44:29 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,30 @@
 
+#Given variables
+party_pizza_mini = 14
+large = 8
+medium = 6
+people = 6 # friends
+
+#Do Not use crtl+A to select code.  Only copy the code below this line.
+#------------------------------------------------------------------------------------------------
+total_slices: 28
+slices * people
+
+total_leftover
+print(f"total_leftover: {slices}")
+
+
+total_people * 2
+share = slices
+total = slices
+share_slices = total
+
+
+#Mom says "Wait, Brandon’s coming. We’re going to need more pizza. I’ll upgrade the mini to a party_pizza instead. It’s the same as 2 minis. Hopefully the leftovers will be enough to fill his hollow leg.”
+
+# TODO Update slices
+# TODO Recalculate share
+# TODO Recalculate leftover
+# TODO Print share result
+# TODO Print leftover result
+print("...for Mr. Hollow Leg
